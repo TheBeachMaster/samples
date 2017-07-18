0456ab7e60b3b7393a86d0de1a799ffda3f3ee53
TheBeachmaster <otienoken@outlook.com>
2017-07-18 17:50:26 +0300

    UInt16 Conversion

--- types.c
+++ types.c
@@ -3,23 +3,17 @@
 #include "string.h"
 
 
-int datai = 254;
+uint16_t datai = 254;
 
 char mydata [41];
 
-char inToChar(int val)
-{
-    char udata [41];
-    sprintf(udata,"%d",val);
-    return udata;
-}
+
 
 int main()
 {
     printf("Getting started...\n");
     sprintf(mydata,"%d",datai);
     printf("Your int is %i ,your char is %s\n",datai,mydata);
-    mydata = inToChar(datai);
-    printf("Your func data is %s\n",mydata);
+   
     return 0;
 }
