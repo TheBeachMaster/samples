40be975b8c1da80ace7ef094584c44b22798844d
TheBeachmaster <otienoken@outlook.com>
2017-07-18 17:44:40 +0300

    Functional converter for itoa

--- types.c
+++ types.c
@@ -7,11 +7,19 @@ int datai = 254;
 
 char mydata [41];
 
+char inToChar(int val)
+{
+    char udata [41];
+    sprintf(udata,"%d",val);
+    return udata;
+}
 
 int main()
 {
     printf("Getting started...\n");
     sprintf(mydata,"%d",datai);
     printf("Your int is %i ,your char is %s\n",datai,mydata);
+    mydata = inToChar(datai);
+    printf("Your func data is %s\n",mydata);
     return 0;
 }
