c658c9d9d764a7c53a0b9f0a7546066ea9b81cc8
TheBeachmaster <otienoken@outlook.com>
2017-08-07 11:46:23 +0300

    Fresh code for Synchronous Subscriber

--- SyncSub1.c
+++ SyncSub1.c
@@ -6,7 +6,7 @@
 #define ADDRESS     "tcp://sungura1-angani-ke-host.africastalking.com:1883"
 #define CLIENTID    "EdisonSync"
 #define TOPIC       "Host"
-#define PAYLOAD     "Hello Edison"
+//#define PAYLOAD     "Hello Edison"
 #define QOS         1
 #define TIMEOUT     10000L
 
@@ -17,33 +17,18 @@ int main(int argc,char * arg[])
     MQTTClient client;
     MQTTClient_message  message = MQTTClient_message_initializer;
     MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
-    // MQTTClient_message pubmsg = MQTTClient_message_initializer;
-    MQTTClient_deliveryToken token;
     int rc;
+    int topicLen;
     int ch;
-
-    MQTTClient_create(&client, ADDRESS, CLIENTID,
-                      MQTTCLIENT_PERSISTENCE_NONE, NULL);
+    MQTTClient_create(&client, ADDRESS, CLIENTID,MQTTCLIENT_PERSISTENCE_NONE, NULL);
     conn_opts.keepAliveInterval = 20;
     conn_opts.cleansession = 1;
-    if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS)
+
+    do
     {
-        printf("Failed to connect, return code %d\n", rc);
-        exit(EXIT_FAILURE);
-    }
-    MQTTClient_subscribe(client, TOPIC, QOS);
-    MQTTClient_receive(client,TOPIC,strlen(PAYLOAD),&message,TIMEOUT);
-
-    printf("Received... %s\n",(char*)message->payload);
-
-    rc = MQTTClient_waitForCompletion(client, token, TIMEOUT);
-    
-    do 
-    {
-        ch = getchar();
-    } while(ch!='Q' && ch != 'q');
-    MQTTClient_disconnect(client, 10000);
-    MQTTClient_destroy(&client);
-
+        MQTTClient_receive(client,&TOPIC,&topicLen,&message,2000);
+        printf("Received Data from %s: \n",TOPIC);
+        printf("Contents are %s \n",(char*)(message->payload));
+    }while((rc = MQTTClient_connect(client,&conn_opts)) && (rc = MQTTClient_subscribe(client,TOPIC,QOS))) == MQTTCLIENT_SUCCESS)
     return rc;
 }
