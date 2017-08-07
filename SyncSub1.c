ec0785eb24c79fa608c317c68605a0025e54ed26
TheBeachmaster <otienoken@outlook.com>
2017-08-07 12:18:59 +0300

    Fixed Syntax errors, NPEs,Bad Addresses etc

--- SyncSub1.c
+++ SyncSub1.c
@@ -5,7 +5,7 @@
 
 #define ADDRESS     "tcp://sungura1-angani-ke-host.africastalking.com:1883"
 #define CLIENTID    "EdisonSync"
-#define TOPIC       "Host"
+//#define TOPIC       "Host"
 //#define PAYLOAD     "Hello Edison"
 #define QOS         1
 #define TIMEOUT     10000L
@@ -15,10 +15,11 @@
 int main(int argc,char * arg[])
 {
     MQTTClient client;
-    MQTTClient_message  message = MQTTClient_message_initializer;
+    MQTTClient_message*  message = MQTTClient_message_initializer;
     MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
     int rc;
     int topicLen;
+    char * TOPIC = "Host";
     int ch;
     MQTTClient_create(&client, ADDRESS, CLIENTID,MQTTCLIENT_PERSISTENCE_NONE, NULL);
     conn_opts.keepAliveInterval = 20;
@@ -28,10 +29,10 @@ int main(int argc,char * arg[])
     {
         MQTTClient_receive(client,&TOPIC,&topicLen,&message,2000);
         printf("Received Data from %s: \n",TOPIC);
-        printf("Contents are %s \n",(char*)(message->payload));
+        printf("Contents are %s \n",(char*)(message->payload);
         MQTTClient_freeMesage(&message);
-        MQTTClient_free(&topic);
-    }while((rc = MQTTClient_connect(client,&conn_opts)) && (rc = MQTTClient_subscribe(client,TOPIC,QOS))) == MQTTCLIENT_SUCCESS)
+        MQTTClient_free(&TOPIC);
+    }while((rc = MQTTClient_connect(client,&conn_opts)) && (rc = MQTTClient_subscribe(client,TOPIC,QOS)) == MQTTCLIENT_SUCCESS)
     MQTTClient_disconnect(client,10000);
     MQTTClient_destroy(&client);
     return rc;
