4bedf10a4c2815d5d7d1d6e04fa585e867bcfc56
TheBeachmaster <otienoken@outlook.com>
2017-09-29 16:48:04 +0300

    Adding Kapua mongo client logic

--- mongo/mqttclient.js
+++ mongo/mqttclient.js
@@ -4,21 +4,25 @@ var assert = require('assert');
 var dbUser = "localuser";
 var dbPass = "localpassword";
 var connectionString = `mongodb://${dbUser}:${dbPass}@ds139954.mlab.com:39954/mqttdatastore`;
-var rootTopic = "demo/nodeclient";
 var collection, client;
-client = mqttClient.connect("mqtt://sungura1-angani-ke-host.africastalking.com:1882");
+var options = {
+    username: "kapua-broker",
+    password: "kapua-password"
+};
+client = mqttClient.connect("mqtt://sungura1-angani-ke-host.africastalking.com:1883", options);
 client.on('connect', function() {
     console.log("Connected successfully to broker");
-    client.subscribe('demo/nodeclient/presence');
     mongoClient.connect(connectionString, function(err, db) {
         collection = db.collection('messages');
         assert.equal(null, err);
         console.log("Connected successfully to server");
     });
+    client.subscribe('kapua-sys/ESP8266/sensors');
 });
 client.on('message', function(topic, message) {
     // message is Buffer
     console.log(message.toString());
+    var data = message.toString();
     // client.end();
-    collection.insertOne({ messages: message.toString() });
+    collection.insertOne({ messages: data, time: Date() });
 });
