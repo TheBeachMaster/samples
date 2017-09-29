df20e727c1b196a82099812a1b2662b3149dd695
TheBeachmaster <otienoken@outlook.com>
2017-09-29 16:46:02 +0300

    MFR code refactorings

--- arduino/mfr/mfrdata.ino
+++ arduino/mfr/mfrdata.ino
@@ -5,7 +5,7 @@
 #define RFID_SS          9         
 MFRC522 rfid(RFID_SS, RFID_RST);
 ///Checks for an Associated Tag
-void asscociatedTag();
+int asscociatedTag(char cardNum, char* cardBank[],char* assocNum[]);
 //Initializes the RFID Library
 void rfidInit();
 //Read RFID UID
@@ -18,18 +18,21 @@ char _tagSwiped=0;
 byte readCard[7]; // Array to store UID of a Single Tag temporarily
 
 char* uidBank[] ={
-    "A85EE927",
-    "C4934A49",
-    "B6902EAC"
+    "a85ee927", //A85EE927
+    "c4934a49", //C4934A49
+    "b6902eac" //B6902EAC
 };
 
 char* assocUID[] ={
     "+254724587654",
-    "+254724587654",
-    "+254724587654"
+    "+254712384171",
+    "+254703415953"
 };
 
-int availableTags = sizeof(uidBank)/sizeof(uidBank[0]);
+uint8_t availableTags = sizeof(uidBank)/sizeof(uidBank[0]);
+int isCardAvailable(char* storageBank[],char cardVal[10]);
+
+
 
 void setup()
 {
@@ -43,6 +46,27 @@ void setup()
     Serial.println("Setting Up");
 	
 }
+ int isCardAvailable(char* storageBank[],char cardVal[10])
+ {
+    int i = 0;
+    char* tagsBank[100];
+    char cardNumber[10];
+    storageBank = tagsBank;
+    cardVal = cardNumber ;
+    int tags = sizeof(storageBank)/sizeof(storageBank[0]);
+     for (i; i < tags; i++)
+     {
+         if (tagsBank[i] == cardNumber)
+         {
+             return tags;
+         }
+     }
+     if (i == availableTags)
+     {
+        return -1;
+     }
+    
+ }
 
 void loop()
 {
@@ -60,10 +84,6 @@ void loop()
 	
 }
 
-void asscociatedTag()
-{
-
-}
  void rfidInit()
  {
     pinMode(RFID_SS, OUTPUT);
@@ -107,8 +127,56 @@ void asscociatedTag()
     }
     Serial.print("card buffer : ");
     Serial.println(card_buffer);
+    int response = isCardAvailable(uidBank,card_buffer);
+    Serial.println(response);
+    // if (isCardAvailable(uidBank,card_buffer))
+    // {
+    //     Serial.println("Found it");
+    // }else
+    // {
+    //     Serial.println("Not found");
+    // }
+    // // int i = 0;
+
+    // for (i; i < availableTags; i++)
+    // {
+    //     if (uidBank[i] == card_buffer)
+    //     {
+    //         Serial.println("Found");
+    //     }
+    // }
+    // if (i == availableTags)
+    // {
+    //     Serial.println("Not found");
+    // }
+    // if (uidBank[0] == card_buffer)
+    // {
+    //     Serial.println("Found");
+    
+    // }
+   
+    // if (card_buffer == uidBank[i])
+    // {
+    //     Serial.println("Found!");
+       
+    // }
+    // for (int i = 0; i < availableTags; i++)
+    // {
+    //     // Serial.print(F("Bank number "));
+    //     // Serial.print(i);
+    //     // Serial.print(F(" : "));
+    //     // Serial.print(uidBank[i]);
+    //     // Serial.print(F("is associated with : "));
+    //     // Serial.println(assocUID[i]);
+    //     if (card_buffer == uidBank[i])
+    //     {
+    //         Serial.print("Card Found at");
+    //         Serial.println(uidBank[i]);
+    //        // return 0;
+    //     }
+    // }
     rfid.PICC_HaltA(); // Stop reading
-    return 1;
+    return 0;
  }
  uint8_t tagSwiped(){
     rfidInit();
